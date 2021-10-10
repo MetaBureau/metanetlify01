08f8ff1af0bcc51f3895fa8a4c2ee9c3bf233fa9
Stewart Milne <srm@metabureau.com.au>
2021-10-11 10:47:29 +1100

    USP

--- pages/index.vue
+++ pages/index.vue
@@ -3,6 +3,9 @@
     <div class="container is-mobile has-text-centered">
       <h1 class="title is-2">MetaBureau</h1>
       <h2 class="title is-2">Information Architecture consulants</h2>
+      <h3 class="title is-3">
+        Technical, strategic and creative design services.
+      </h3>
     </div>
   </section>
 </template>
