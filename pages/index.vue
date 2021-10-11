f8cb627014ea6cf9f68bd7dcddfca3891389e1f8
Stewart Milne <srm@metabureau.com.au>
2021-10-11 13:34:22 +1100

    service list

--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,37 @@
 <template>
   <section class="section">
-    <div class="container is-mobile has-text-centered">
+    <div class="container is-mobile">
       <h1 class="title is-2">MetaBureau</h1>
-      <h2 class="title is-2">Information Architecture consulants</h2>
+      <h2 class="title is-2">Information Architecture Consulants</h2>
       <h3 class="title is-3">
-        Technical, strategic and creative design services.
+        Technical, strategic and creative digital design services
       </h3>
+      <dl>
+        <dt class="subtitle is-3">Information Architecture</dt>
+        <dd>Hierarchy, taxonomy, data types ...</dd>
+        <dt class="is-size-3">Audit and Review</dt>
+        <dd class="is-size-4 mb-4">
+          Survey of existintg digital presence and assessment with regard to
+          organisation goals.
+        </dd>
+        <dt class="is-size-3">Information Architecture</dt>
+        <dd class="is-size-4 mb-4">Hierarchy, taxonomy, data types ...</dd>
+        <dt class="is-size-3">Rapid Iterative Prototyping</dt>
+        <dd class="is-size-4 mb-4">
+          Development of navigable prototypes to assess performane of design
+          prior to commiting to a full bulid.
+        </dd>
+        <dt class="is-size-3">Web Site Design and Development</dt>
+        <dd class="is-size-4 mb-4">
+          Design and development of websites in various platforms including
+          Drupal, Laravel, Nuxt.
+        </dd>
+        <dt class="is-size-3">eCommerce Development</dt>
+        <dd class="is-size-4 mb-4">
+          Design and development of eCommerce websites in various platfroms
+          including Shopify, Magento, Big Commerce, ECWID.
+        </dd>
+      </dl>
     </div>
   </section>
 </template>
