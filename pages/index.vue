e7b0f35a1b5bf2fcc730f2b3023c9621a955e2f9
Stewart Milne <srm@metabureau.com.au>
2022-05-27 11:50:17 +1000

    smaller fonts

--- pages/index.vue
+++ pages/index.vue
@@ -1,14 +1,14 @@
 <template>
   <div>
-    <section class="hero is-medium is-primary">
+    <section class="hero is-small is-primary">
       <div class="hero-body">
         <div class="columns is-desktop">
           <div class="column">
-            <h1 class="title is-1">
+            <h1 class="title is-2">
               MetaBureau: A mulitdisciplinary digital consultancy
             </h1>
-            <h2 class="title is-2">Information Architecture Consulants</h2>
-            <h3 class="title is-3">
+            <h2 class="title is-4">Information Architecture Consulants</h2>
+            <h3 class="title is-4">
               Technical, strategic and creative digital design services
             </h3>
           </div>
@@ -46,36 +46,36 @@
               </div>
             </div>
             -->
-            <h3 class="title is-3">Services include ...</h3>
+            <h3 class="title is-4">Services include ...</h3>
             <dl>
-              <dt class="is-size-3">Audit and Review</dt>
-              <dd class="is-size-4 mb-4">
+              <dt class="is-size-4">Audit and Review</dt>
+              <dd class="is-size-5 mb-4">
                 Survey of existing digital presence and assessment with regard
                 to organisational goals.
               </dd>
-              <dt class="is-size-3">Information Architecture</dt>
-              <dd class="is-size-4 mb-4">
+              <dt class="is-size-4">Information Architecture</dt>
+              <dd class="is-size-5 mb-4">
                 Modelling and specification of hierarchy, taxonomy, data-types.
               </dd>
-              <dt class="is-size-3">Rapid Iterative Prototyping</dt>
-              <dd class="is-size-4 mb-4">
+              <dt class="is-size-4">Rapid Iterative Prototyping</dt>
+              <dd class="is-size-5 mb-4">
                 Development of navigable prototypes to assess performance of
                 design prior to commiting to a full bulid.
               </dd>
-              <dt class="is-size-3">Web Site Design and Development</dt>
-              <dd class="is-size-4 mb-4">
+              <dt class="is-size-4">Web Site Design and Development</dt>
+              <dd class="is-size-5 mb-4">
                 Design and development of websites in various platforms
                 including Drupal, Laravel, Nuxt.
               </dd>
-              <dt class="is-size-3">eCommerce Development</dt>
-              <dd class="is-size-4 mb-4">
+              <dt class="is-size-4">eCommerce Development</dt>
+              <dd class="is-size-5 mb-4">
                 Design and development of eCommerce websites in various
                 platfroms including Shopify, Magento, Big Commerce, ECWID.
               </dd>
-              <dt class="is-size-3">
+              <dt class="is-size-4">
                 Oversight and service procurement management
               </dt>
-              <dd class="is-size-4 mb-4">
+              <dd class="is-size-5 mb-4">
                 Assistance with management and procurement of third party
                 service providers such as graphic designers, software developers
                 and SEO consulatants.
